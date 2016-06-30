8813cf65742aa39399c3e7d51fd4e05f6940fad3
dudusbarros <joseeduardobarros@gmail.com>
2016-06-30 09:53:01 -0300

    Add fullscreen controllers

--- js/app.js
+++ js/app.js
@@ -9,12 +9,64 @@ var App = (function () {
 	 */
 	function App(element, base) {
 
+		var self = this;
+
 		this.element = element;
 		this.base = base;
 
 		this.VotingBox = new VotingBox(document.getElementById('voting-box'), this.base);
 
+		this.header = document.getElementById('ufs-app-header');
+
+		this.fullscreenCtrl = function () {
+
+			self.requestFullScreen();
+
+		};
+
+		this.isFullscreen = false
+
+		this.init();
+
 	}
+
+	App.prototype.requestFullScreen = function () {
+
+		if (!this.isFullscreen) {
+
+			// go to fullscreen
+			if (document.documentElement.requestFullscreen) {
+				document.documentElement.requestFullscreen();
+			} else if(document.documentElement.mozRequestFullScreen) {
+				document.documentElement.mozRequestFullScreen();
+			} else if(document.documentElement.webkitRequestFullscreen) {
+				document.documentElement.webkitRequestFullscreen();
+			} else if(document.documentElement.msRequestFullscreen) {
+				document.documentElement.msRequestFullscreen();
+			}
+
+		} else {
+
+			// exit full fullscreen
+			if (document.exitFullscreen) {
+				document.exitFullscreen();
+			} else if(document.mozCancelFullScreen) {
+				document.mozCancelFullScreen();
+			} else if(document.webkitExitFullscreen) {
+				document.webkitExitFullscreen();
+			}
+
+		}
+
+		this.isFullscreen = !this.isFullscreen;
+
+	};
+
+	App.prototype.init = function () {
+
+		this.header.addEventListener('click', this.fullscreenCtrl);
+
+	};
 
 	return App;
 
